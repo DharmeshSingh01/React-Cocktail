47bc25996a19dfa6c56170656c2661b30b630d57
Dharmesh Singh <63810275+DharmeshSingh01@users.noreply.github.com>
2022-09-26 21:35:05 +0530

    fixed Router bug

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/about' element={<About />} />
-        <Route exact path='/cocktail:id' element={<SingleCocktail />} />
+        <Route exact path='/cocktail/:id' element={<SingleCocktail />} />
         <Route exact path='/*' element={<Error />} />
       </Routes>
     </Router>
